fd8be0008dc50ccb589f24ef3093bf8029d873a2
sunchulbaek <sunchulbaek@ncsoft.com>
2022-07-04 16:44:42 +0900

    검색 동작 개선

--- app/src/main/java/kr/pe/ssun/androiddemo/ui/main/MainFragment.kt
+++ app/src/main/java/kr/pe/ssun/androiddemo/ui/main/MainFragment.kt
@@ -1,10 +1,12 @@
 package kr.pe.ssun.androiddemo.ui.main
 
 import android.os.Bundle
+import android.view.KeyEvent
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
@@ -12,7 +14,6 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
-import kr.pe.ssun.androiddemo.R
 import kr.pe.ssun.androiddemo.databinding.FragmentMainBinding
 
 @AndroidEntryPoint
@@ -26,6 +27,22 @@ class MainFragment : Fragment() {
     ): View {
         val navController = findNavController()
         binding = FragmentMainBinding.inflate(inflater, container, false).apply {
+            et.setOnEditorActionListener { tv, actionId, keyEvent ->
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    viewModel.getShop(et.text.toString())
+                    et.text = null
+                    true
+                }
+                false
+            }
+            et.setOnKeyListener { view, keyCode, keyEvent ->
+                if (keyCode == KeyEvent.KEYCODE_ENTER) {
+                    viewModel.getShop(et.text.toString())
+                    et.text = null
+                    true
+                }
+                false
+            }
             btn.setOnClickListener {
                 viewModel.getShop(et.text.toString())
                 et.text = null
